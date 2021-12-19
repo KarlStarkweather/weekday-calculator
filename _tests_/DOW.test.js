ebf56993fc1df6ef0fb839e0e79a19a6ca4510d0
KarlStarkweather <stark13@usa.net>
2021-12-19 11:22:24 -0800

    generate a date object from a standard string mm/dd/yyyy

--- _tests_/DOW.test.js
+++ _tests_/DOW.test.js
@@ -2,9 +2,23 @@ import Day from '../src/DOW.js';
 
 describe('Day', () => {
 
-  test('should correctly create a date object from a string', () => {
-    const inputtedDate = new Date(2021,11,1);
+  test('should correctly create a date object from inputted elements', () => {
+    const inputtedDate = new Date(2021,11,19);
     expect(inputtedDate.getFullYear()).toEqual(2021);
+    expect(inputtedDate.getMonth()).toEqual(11);
+    expect(inputtedDate.getDate()).toEqual(19);
+  });
+  test('should correctly identify the DOW', () => {
+    const inputtedDate = new Date(2021,11,19);
+    expect(inputtedDate.getDay()).toEqual(0);
+  });
+  test('should generate a date object from a standard string', () => {
+    const dateString = "12/19/2021";
+    let inputtedDate = Day(dateString);
+    expect(inputtedDate.getFullYear()).toEqual(2021);
+    expect(inputtedDate.getMonth()).toEqual(11);
+    expect(inputtedDate.getDate()).toEqual(19);
+    
   });
 
 });
--- src/DOW.js
+++ src/DOW.js
@@ -1,3 +1,14 @@
-export default function Day(date) {
-  this.side3 = side3;
+export default function Day(dateString) {
+  // expect standard format like "12/15/2021"
+  let year, month, day;
+  let dArray = dateString.split("/");
+  dArray.forEach(function(element) {
+    element = parseInt(element);
+  });
+  dArray[0] --;
+  year = dArray[2];
+  month = dArray[0];
+  day = dArray[1];
+  const inputtedDate = new Date(year,month,day);
+  return inputtedDate;
 }
