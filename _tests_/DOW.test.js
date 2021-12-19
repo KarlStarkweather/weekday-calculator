47ef588073b7464af525d400cf43f6cdecc2880c
KarlStarkweather <stark13@usa.net>
2021-12-19 14:26:53 -0800

    return the weekday as a string after passing in the date as a string

--- _tests_/DOW.test.js
+++ _tests_/DOW.test.js
@@ -1,24 +1,28 @@
 import Day from '../src/DOW.js';
 
 describe('Day', () => {
-
-  test('should correctly create a date object from inputted elements', () => {
-    const inputtedDate = new Date(2021,11,19);
-    expect(inputtedDate.getFullYear()).toEqual(2021);
-    expect(inputtedDate.getMonth()).toEqual(11);
-    expect(inputtedDate.getDate()).toEqual(19);
+  test('should correctly make a Day object that has a day, month and year property', () =>{
+    const newDay = new Day('12/1/2021');
+    expect(newDay.year).toEqual(2021);
+    expect(newDay.day).toEqual(1);
+    expect(newDay.month).toEqual(12);
   });
-  test('should correctly identify the DOW', () => {
-    const inputtedDate = new Date(2021,11,19);
-    expect(inputtedDate.getDay()).toEqual(0);
+  test('should correctly create a date object from the day, month and year', () => {
+    const newDay = new Day('12/1/2021');
+    const testDate = new Date(2021, 11, 1);
+    expect(newDay.createDate()).toEqual(testDate);
   });
-  test('should generate a date object from a standard string', () => {
-    const dateString = "12/19/2021";
-    let inputtedDate = Day(dateString);
-    expect(inputtedDate.getFullYear()).toEqual(2021);
-    expect(inputtedDate.getMonth()).toEqual(11);
-    expect(inputtedDate.getDate()).toEqual(19);
-    
+  test('should return a string of the weekday', () => {
+    const newDay = new Day('12/19/2021');
+    expect(newDay.DOW()).toEqual("Sunday");
+  });
+  test('should return work for future dates', () => {
+    const newDay = new Day('12/25/2021');
+    expect(newDay.DOW()).toEqual("Saturday");
+  });
+  test('should return work for past dates', () => {
+    const newDay = new Day('11/25/2021');
+    expect(newDay.DOW()).toEqual("Thursday");
   });
 
 });
--- src/DOW.js
+++ src/DOW.js
@@ -1,14 +1,32 @@
 export default function Day(dateString) {
   // expect standard format like "12/15/2021"
-  let year, month, day;
   let dArray = dateString.split("/");
-  dArray.forEach(function(element) {
-    element = parseInt(element);
-  });
-  dArray[0] --;
-  year = dArray[2];
-  month = dArray[0];
-  day = dArray[1];
-  const inputtedDate = new Date(year,month,day);
-  return inputtedDate;
+  
+  this.year = parseInt(dArray[2]);
+  console.log(this.year);
+  this.month = parseInt(dArray[0]);
+  this.day = parseInt(dArray[1]);
+};
+
+Day.prototype.createDate = function() {
+  let testMonth = this.month - 1;
+  let targetDate = new Date(this.year, testMonth,this.day);
+  return targetDate;
 }
+
+Day.prototype.DOW = function() {
+  const weekDays = [
+    [0,"Sunday"],
+    [1,"Monday"],
+    [2,"Tuesday"],
+    [3,"Wednesay"],
+    [4,"Thursday"],
+    [5,"Friday"],
+    [6,"Saturday"],
+  ];
+  
+  return weekDays[this.createDate().getDay()][1];
+};
+
+
+
